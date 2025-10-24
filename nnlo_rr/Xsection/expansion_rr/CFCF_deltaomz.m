9a4534e807bca03f06b70da12e3ef17837af5e36
cesarecarlo-mella <118001774+cesarecarlo-mella@users.noreply.github.com>
2025-10-24 16:16:26 +0100

    fit iterated integrals to classical polylogs for qg channel.

--- nnlo_rr/Xsection/expansion_rr/CFCF_deltaomz.m
+++
@@ -1,8 +0,0 @@
-(* Created with the Wolfram Language : www.wolfram.com *)
-(-2*delta[1 - y]*delta[1 - z] - 2*delta[y]*delta[1 - z])/ep^3 + 
- (2*delta[1 - y]*delta[1 - z] + 2*delta[y]*delta[1 - z])/ep^4 + 
- ((-7*Pi^2*delta[1 - y]*delta[1 - z])/3 - (7*Pi^2*delta[y]*delta[1 - z])/3)/
-  ep^2 + ((delta[1 - y]*delta[1 - z]*(7*Pi^2 - 124*Zeta[3]))/3 + 
-   (delta[y]*delta[1 - z]*(7*Pi^2 - 124*Zeta[3]))/3)/ep + 
- delta[1 - y]*delta[1 - z]*(-1/20*Pi^4 + (124*Zeta[3])/3) + 
- delta[y]*delta[1 - z]*(-1/20*Pi^4 + (124*Zeta[3])/3)
--- nnlo_rr/Xsection/ids/idsQ1.m
+++
@@ -1,9 +0,0 @@
-(* Created with the Wolfram Language : www.wolfram.com *)
-{G[(r*t)/(-1 + r*t), 1] -> -Log[r] - Log[t], G[(2*r*t)/(-1 + r*t), 1] -> 
-  -Log[2] - Log[r] - Log[t] + Log[1 + r*t], G[(r^2 + r*t)/(-1 + r*t), 1] -> 
-  -Log[r] + Log[1 + r^2] - Log[r + t], G[t^2/(-1 + t^2), 1] -> -2*Log[t], 
- G[(2*t^2)/(-1 + t^2), 1] -> -Log[2] - 2*Log[t] + Log[1 + t^2], 
- G[0, (2*t^2)/(-1 + t^2), 1] -> G[0, (2*t^2)/(-1 + t^2), 1], 
- G[t^2/(-1 + t^2), 0, 1] -> G[t^2/(-1 + t^2), 0, 1], 
- G[t^2/(-1 + t^2), (2*t^2)/(-1 + t^2), 1] -> 
-  G[t^2/(-1 + t^2), (2*t^2)/(-1 + t^2), 1]}
